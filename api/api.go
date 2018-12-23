329ea5baa581cd956252ba57325656c1b8190ebb
medusar <exceptionplayer@gmail.com>
2018-12-23 17:33:24 +0800

    add api

--- api/api.go
+++ api/api.go
@@ -5,18 +5,27 @@ import (
 	"log"
 )
 
+const CookieName = "sessionid"
+
 type UserApi struct {
 }
 
 func (userApi *UserApi) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//cookie, err := r.Cookie(CookieName)
+	//if err != nil {
+	//	w.WriteHeader(http.StatusUnauthorized)
+	//	return
+	//}
+	//TODO: encrypt cookie
+	//sessionid := cookie.Value
+
+
 	method := r.Method
 	requestURI := r.RequestURI
 	log.Println("request uri:", requestURI, ", method:", method)
 
 	r.ParseForm()
-
-	form := r.Form
-	log.Printf("request form: %+v", form)
+	//form := r.Form
 
 }
 
